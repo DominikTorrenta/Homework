3630c04358c1788fe14351dae4755a07d3664d16
jorik0508 <goshayan01@gmail.com>
2022-04-13 00:08:53 +0300

    Домашняя работа номер четыре доделанная до конца

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,37 @@
+fun main(args: Array<String>) {
+
+    val BookOne : Book?= null
+    val Booktwo= Book(price = 10, wordCount = 150)
+
+    BookOne?.let{
+        buy(it)
+    }
+    Booktwo.let{
+        buy(it)
+    }
+}
 interface Publication {
     val price : Int
     val wordCount : Int
     fun getType (): String
 }
 
-fun main(args: Array<String>) {
-    buy(publication = Publication)
 
-    abstract class Book : Publication {
-        val page1: String? = null
-        val page2: String? = "Hi"
-        override fun getType(): String {
-            return if (wordCount<=1000) {
-                ("Flash Fiction")
-            } else{if (wordCount<=7500) {
-                ("Short Story")
-            } else ("Novel")
-            }
-        }
-    }
 
-    abstract class Magazine: Publication {
-        override fun getType(): String {
-            return "Magazine"
+fun buy(publication: Publication?) {
+    println("The purchase is complete. The purchase amount was ${publication?.price}")
+}
+class Book(override val price: Int, override val wordCount: Int) : Publication {
+    override fun getType(): String =
+        when{
+            wordCount<=1000 -> "Flash Fiction"
+            wordCount<=7500 -> "Short Story"
+            else -> "Novel"
         }
-    }
 }
 
-fun buy(publication: String) {
-    println("The purchase is complete")
+class Magazine(override val price: Int, override val wordCount: Int) : Publication {
+    override fun getType(): String {
+        return "Magazine"
+    }
 }
